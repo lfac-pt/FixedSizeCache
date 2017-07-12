df2afbe302d5c14756b0db45e2d324806f8b20ce
Luis Cardoso <me@lfac.me>
2017-07-12 18:25:49 +0100

    Adds jsdoc

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const LRU = require('lru-cache');
 
-class FixedSizeCache2 {
+class FixedSizeCache {
 	constructor(options) {
 		if (options && options.settings && options.settings.maxCacheSizeBytes < Infinity) {
 			this._maxCacheSizeBytes = options.settings.maxCacheSizeBytes;
@@ -18,12 +18,23 @@ class FixedSizeCache2 {
 		this._state = new Map();
 	}
 
+	/**
+     * Stores a value in the entry with name key.
+     * @param {String} key
+     * @param {*} value
+     * @returns {FixedSizeCache}
+     */
 	set(key, value) {
 		this._lru.set(key, JSON.stringify(value));
 
 		return this;
 	}
 
+    /**
+     * Gets the value associated with key, if it exists, otherwise returns undefined.
+     * @param {String} key
+     * @returns {*}
+     */
 	get(key) {
 		const value = this._lru.get(key);
 
@@ -34,23 +45,41 @@ class FixedSizeCache2 {
 		return undefined;
 	}
 
+    /**
+     * Returns true if for the key there is an associated value.
+     * @param {String} key
+     * @returns {boolean}
+     */
 	has(key) {
 		return this._lru.has(key);
 	}
 
+    /**
+     * Removes the value associated with the and the key itself.
+     * @param {String} key
+     * @returns {FixedSizeCache}
+     */
 	remove(key) {
 		this._lru.del(key);
 
 		return this;
 	}
 
+    /**
+     * Returns the remaining free space in the cache in bytes
+     * @returns {number}
+     */
 	getCacheMaximumCapacityInBytes() {
 		return this._maxCacheSizeBytes;
 	}
 
+    /**
+     * Returns the cache maximum capacity in bytes
+     * @returns {number}
+     */
 	getRemainingBytes() {
 		return this._maxCacheSizeBytes - this._lru.length;
 	}
 }
 
-module.exports = FixedSizeCache2;
+module.exports = FixedSizeCache;
